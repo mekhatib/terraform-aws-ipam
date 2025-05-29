d2f78ea09a929026c0122f87d24c1a7d5296b684
mekhatib <melkhatib@ziggo.nl>
2025-05-30 00:55:29 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,10 @@
+provider "aws" {
+  region = var.region
+}
+
 data "aws_region" "current" {}
 
-# 1. Create IPAM
+# IPAM Instance
 resource "aws_vpc_ipam" "main" {
   description = "${var.project_name} IPAM"
 
@@ -8,30 +12,36 @@ resource "aws_vpc_ipam" "main" {
     region_name = data.aws_region.current.name
   }
 
-  tags = merge(var.tags, {
-    Name = "${var.project_name}-${var.environment}-ipam"
-  })
+  tags = merge(
+    var.tags,
+    {
+      Name = "${var.project_name}-${var.environment}-ipam"
+    }
+  )
 }
 
-# 2. Create Parent Pool
+# Parent IPAM Pool
 resource "aws_vpc_ipam_pool" "main" {
   address_family = "ipv4"
   ipam_scope_id  = aws_vpc_ipam.main.private_default_scope_id
   description    = "${var.project_name} main pool"
 
-  tags = merge(var.tags, {
-    Name = "${var.project_name}-${var.environment}-ipam-pool"
-    Type = "parent"
-  })
+  tags = merge(
+    var.tags,
+    {
+      Name = "${var.project_name}-${var.environment}-ipam-pool"
+      Type = "parent"
+    }
+  )
 }
 
-# 3. Provision a CIDR block into the parent pool
+# Provision CIDR to the Parent Pool
 resource "aws_vpc_ipam_pool_cidr" "main" {
   ipam_pool_id = aws_vpc_ipam_pool.main.id
   cidr         = var.ipam_pool_cidr # Example: "10.0.0.0/8"
 }
 
-# 4. Create VPC Pool (child of main pool)
+# Child Pool (for VPCs or Subnets)
 resource "aws_vpc_ipam_pool" "vpc" {
   address_family      = "ipv4"
   ipam_scope_id       = aws_vpc_ipam.main.private_default_scope_id
@@ -43,31 +53,13 @@ resource "aws_vpc_ipam_pool" "vpc" {
   allocation_min_netmask_length     = 16
   allocation_max_netmask_length     = 28
 
-  tags = merge(var.tags, {
-    Name = "${var.project_name}-${var.environment}-vpc-pool"
-    Type = "vpc"
-  })
-}
-
-# 5. Allocate CIDR from parent pool
-resource "aws_vpc_ipam_pool_cidr_allocation" "vpc_from_parent" {
-  ipam_pool_id   = aws_vpc_ipam_pool.main.id
-  netmask_length = 16
+  tags = merge(
+    var.tags,
+    {
+      Name = "${var.project_name}-${var.environment}-vpc-pool"
+      Type = "vpc"
+    }
+  )
 
   depends_on = [aws_vpc_ipam_pool_cidr.main]
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-# 6. Provision the allocated CIDR into the VPC pool
-resource "aws_vpc_ipam_pool_cidr" "vpc" {
-  ipam_pool_id = aws_vpc_ipam_pool.vpc.id
-  cidr         = aws_vpc_ipam_pool_cidr_allocation.vpc_from_parent.cidr
-
-  depends_on = [
-    aws_vpc_ipam_pool_cidr.main,
-    aws_vpc_ipam_pool_cidr_allocation.vpc_from_parent
-  ]
 }

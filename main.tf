5d4efb8c6ebe0039d427d526a07fdd6d8dfb36e3
mekhatib <melkhatib@ziggo.nl>
2025-05-30 01:19:02 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,26 +17,6 @@ resource "aws_vpc_ipam" "main" {
 }
 
 # Parent IPAM Pool
-resource "aws_vpc_ipam_pool" "main" {
-  address_family = "ipv4"
-  ipam_scope_id  = aws_vpc_ipam.main.private_default_scope_id
-  description    = "${var.project_name} main pool"
-
-  tags = merge(
-    var.tags,
-    {
-      Name = "${var.project_name}-${var.environment}-ipam-pool"
-      Type = "parent"
-    }
-  )
-}
-
-# Provision CIDR to the Parent Pool
-resource "aws_vpc_ipam_pool_cidr" "main" {
-  ipam_pool_id = aws_vpc_ipam_pool.main.id
-  cidr         = var.ipam_pool_cidr # Example: "10.0.0.0/8"
-}
-
 # Child Pool (for VPCs or Subnets)
 resource "aws_vpc_ipam_pool" "vpc" {
   address_family      = "ipv4"
@@ -44,11 +24,9 @@ resource "aws_vpc_ipam_pool" "vpc" {
   source_ipam_pool_id = aws_vpc_ipam_pool.main.id
   locale              = data.aws_region.current.name
   description         = "${var.project_name} VPC pool"
-
   allocation_default_netmask_length = 27
   allocation_min_netmask_length     = 16
   allocation_max_netmask_length     = 28
-
   tags = merge(
     var.tags,
     {
@@ -56,16 +34,21 @@ resource "aws_vpc_ipam_pool" "vpc" {
       Type = "vpc"
     }
   )
-
   depends_on = [aws_vpc_ipam_pool_cidr.main]
 }
 
+# Provision CIDR to the Child Pool
+resource "aws_vpc_ipam_pool_cidr" "vpc" {
+  ipam_pool_id = aws_vpc_ipam_pool.vpc.id
+  depends_on   = [aws_vpc_ipam_pool_cidr.main]
+}
+
+# Now you can allocate from the child pool
 resource "aws_vpc_ipam_pool_cidr_allocation" "vpc_from_parent" {
   ipam_pool_id   = aws_vpc_ipam_pool.vpc.id
   netmask_length = 16
-  depends_on     = [aws_vpc_ipam_pool_cidr.main]
+  depends_on     = [aws_vpc_ipam_pool_cidr.vpc]
   lifecycle {
     create_before_destroy = true
   }
 }
-

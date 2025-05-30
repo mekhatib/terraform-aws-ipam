b9f8108ea843393639377ab9b681ea7fbfa52d64
mekhatib <melkhatib@ziggo.nl>
2025-05-30 14:52:34 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,24 @@
 output "ipam_id" {
-  description = "ID of the IPAM"
+  description = "The ID of the IPAM"
   value       = aws_vpc_ipam.main.id
 }
 
 output "main_pool_id" {
-  description = "ID of the main IPAM pool"
+  description = "The ID of the main IPAM pool"
   value       = aws_vpc_ipam_pool.main.id
 }
 
-output "vpc_pool_id" {
-  description = "ID of the VPC IPAM pool"
+output "vpc_ipam_pool_id" {
+  description = "The ID of the VPC IPAM pool"
   value       = aws_vpc_ipam_pool.vpc.id
 }
 
-output "allocated_cidr" {
-  description = "The allocated CIDR block"
-  value       = aws_vpc_ipam_pool_cidr_allocation.vpc_from_parent.cidr
+output "vpc_ipam_pool_cidr" {
+  description = "The CIDR block allocated to the VPC pool"
+  value       = aws_vpc_ipam_pool_cidr.vpc.cidr
+}
+
+output "main_pool_cidr" {
+  description = "The CIDR block of the main pool"
+  value       = aws_vpc_ipam_pool_cidr.main.cidr
 }
